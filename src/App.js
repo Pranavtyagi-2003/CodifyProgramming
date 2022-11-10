4b819dff2fb0addeffee33a227f853f7163f22c5
Pranav <pranav.12014487@lpu.in>
2022-11-11 00:20:54 +0530

    sds

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return ( 
     <div className="App">   
       {/* <VideoFrame/> */}
-      <Router basename="my-website">
+      <Router basename="CodifyProgramming">
          <Routes>
             <Route path='/' element = {<Home/>}></Route>
          </Routes>
