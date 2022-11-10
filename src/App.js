8ea32fb62ffb44d74f276ec0282a35452d6bdb00
Pranav <pranav.12014487@lpu.in>
2022-11-10 23:39:55 +0530

    vbdb

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   return ( 
     <div className="App">
       {/* <VideoFrame/> */}
-      <Router basename='codifyprogramming'>
+      <Router basename='CodifyProgramming'>
          <Routes>
             <Route path='/' element = {<Home/>}></Route>
          </Routes>
